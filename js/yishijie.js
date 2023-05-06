1ddb41ee97270ab99eaa9e522b8fda2894b73190
52670576 <126444808+52670576@users.noreply.github.com>
2023-05-06 12:29:14 +0800

    Update yishijie.js

--- js/yishijie.js
+++ js/yishijie.js
@@ -18,7 +18,7 @@ var rule={
  	// class_name:'BD无修&萝莉&学園&后宫&恋爱&热血&神魔&奇幻&治愈&搞笑&百合冒险&魔法&机战&战争&战斗&犯罪&悬疑&推理&科幻&竞技&运动&耽美&其他&OVA&剧场版&国产动漫',
  	class_name:'日漫&国漫',
  	// class_url:'BD无修&萝莉&学園&后宫&恋爱&热血&神魔&奇幻&治愈&搞笑&百合冒险&魔法&机战&战争&战斗&犯罪&悬疑&推理&科幻&竞技&运动&耽美&其他&OVA&剧场版&国产动漫',
- 	class_url:'area/日本&area/中国',
+ 	class_url:'area/日本&area/大陆',
     cate_exclude:'',
     play_parse:true,
     lazy:'',
